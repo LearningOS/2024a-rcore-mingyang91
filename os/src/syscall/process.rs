39a6b7e52dd19591492997963079ecb693883136
明扬 <my@famer.me>
2024-10-20 22:52:20 +0800

    cherry-pick code from ch5

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -5,12 +5,15 @@ use alloc::sync::Arc;
 use crate::{
     config::MAX_SYSCALL_NUM,
     fs::{open_file, OpenFlags},
-    mm::{translated_refmut, translated_str},
+    mm::{translated_byte_buffer, translated_refmut, translated_str},
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next, TaskStatus,
     },
 };
+
+use crate::timer::{get_time_ms, get_time_us};
+
 
 #[repr(C)]
 #[derive(Debug)]
@@ -117,23 +120,37 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    trace!("kernel: sys_get_time");
+
+    let now = get_time_us();
+    let time_val = TimeVal {
+        sec: now / 1_000_000,
+        usec: now % 1_000_000,
+    };
+
+    copy_to_virt(&time_val, ts);
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TaskInfo`] is splitted by two pages ?
-pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_task_info NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
+    trace!("kernel: sys_task_info");
+    let now = get_time_ms();
+    let new = {
+        let task = current_task()
+            .expect("sys_task_info: current_task failed");
+        let inner = task.inner_exclusive_access();
+        TaskInfo {
+            status: task.status(),
+            time: now - inner.start_time,
+            syscall_times: inner.syscall_times,
+        }
+    };
+    copy_to_virt(&new, ti);
+    0
 }
 
 /// YOUR JOB: Implement mmap.
@@ -181,4 +198,24 @@ pub fn sys_set_priority(_prio: isize) -> isize {
         current_task().unwrap().pid.0
     );
     -1
+}
+
+fn copy_to_virt<T>(src: &T, dst: *mut T) {
+    let src_buf_ptr: *const u8 = unsafe { core::mem::transmute(src) };
+    let dst_buf_ptr: *const u8 = unsafe { core::mem::transmute(dst) };
+    let len = core::mem::size_of::<T>();
+
+    let dst_frames = translated_byte_buffer(
+        current_user_token(),
+        dst_buf_ptr,
+        len
+    );
+
+    let mut offset = 0;
+    for dst_frame in dst_frames {
+        dst_frame.copy_from_slice(unsafe {
+            core::slice::from_raw_parts(src_buf_ptr.add(offset), dst_frame.len())
+        });
+        offset += dst_frame.len();
+    }
 }
